91c73f5f6b5f61ba735098850f0d1ce81a2af448
Emilio Ziniades <emilioziniades@protonmail.com>
2025-10-15 19:17:08 +0200

    setup flake and gh actions for ci

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Recipes served by cook-cli";
+  description = "Cooklang recipe linter";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -18,60 +18,29 @@
           "aarch64-linux"
           "x86_64-darwin"
           "aarch64-darwin"
-        ] (system: fn system nixpkgs.legacyPackages.${system});
-      tag = self.rev or "dirty";
-      port = "8080";
+        ] (system: fn nixpkgs.legacyPackages.${system});
     in
     {
-      packages = forEachSystem (
-        system: pkgs: {
-          container = pkgs.dockerTools.buildLayeredImage {
-            name = "cook-server";
-            tag = tag;
-            contents = [
-              pkgs.cook-cli
-              ./recipes
-            ];
-            config = {
-              Entrypoint = [ "cook" ];
-              Cmd = [
-                "server"
-                "--host"
-                "--port"
-                port
-              ];
-              WorkingDir = ./recipes;
-              ExposedPorts = {
-                port = { };
-              };
-            };
-          };
+      packages = forEachSystem (pkgs: {
+        cooklint = pkgs.rustPlatform.buildRustPackage {
+          name = "cooklint";
+          src = ./cooklint;
+          cargoHash = "sha256-UFMb4i+F87wbpAxe2MSTm53k+UfWSz1sMA4oSbF1cko=";
 
-          cooklint = pkgs.rustPlatform.buildRustPackage {
-            name = "cooklint";
-            src = ./cooklint;
-            cargoHash = "sha256-UFMb4i+F87wbpAxe2MSTm53k+UfWSz1sMA4oSbF1cko=";
-          };
-        }
-      );
+          preBuild = ''
+            cargo check
+          '';
+        };
+      });
 
-      devShells = forEachSystem (
-        system: pkgs: {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              cook-cli
-              skopeo
-              flyctl
-              just
-            ];
-          };
-          lint = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              just
-              self.packages.${system}.cooklint
-            ];
-          };
-        }
-      );
+      devShells = forEachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            just
+            cargo
+            rustc
+          ];
+        };
+      });
     };
 }
